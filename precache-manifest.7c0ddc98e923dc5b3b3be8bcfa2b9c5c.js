0790af8082124cb3ab02c1ad7048085cb92e8723
victor <victor.aefarias@gmail.com>
2019-05-18 01:38:29 -0400

    Updates

---
+++ precache-manifest.7c0ddc98e923dc5b3b3be8bcfa2b9c5c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3198357aed5f396056bf162f24bc9f5e",
+    "url": "/PROpHET/index.html"
+  },
+  {
+    "revision": "9cc17837edc4aa10d604",
+    "url": "/PROpHET/static/css/main.ca053161.chunk.css"
+  },
+  {
+    "revision": "12dface117e5afce0039",
+    "url": "/PROpHET/static/js/2.bb4676a3.chunk.js"
+  },
+  {
+    "revision": "9cc17837edc4aa10d604",
+    "url": "/PROpHET/static/js/main.a3ee3c25.chunk.js"
+  },
+  {
+    "revision": "29580283c1f465a3f923",
+    "url": "/PROpHET/static/js/runtime~main.3fda8091.js"
+  },
+  {
+    "revision": "ec6748d4712d38a795550e86a6434369",
+    "url": "/PROpHET/static/media/brain.ec6748d4.svg"
+  },
+  {
+    "revision": "c0be590a4288576164c46382a233a404",
+    "url": "/PROpHET/static/media/heart.c0be590a.svg"
+  },
+  {
+    "revision": "04daed321a38b1a714835bba541627b9",
+    "url": "/PROpHET/static/media/icecream.04daed32.svg"
+  },
+  {
+    "revision": "793ec6f2862b10fcff2b58fc8ab15e7a",
+    "url": "/PROpHET/static/media/logo.793ec6f2.png"
+  },
+  {
+    "revision": "f8a9769988303409f0c42beabe11fce0",
+    "url": "/PROpHET/static/media/man.f8a97699.svg"
+  },
+  {
+    "revision": "3bbab716ed31657e63e88a5bcdb32641",
+    "url": "/PROpHET/static/media/mind.3bbab716.svg"
+  },
+  {
+    "revision": "c0493b08538378c0e0818294f49f6e45",
+    "url": "/PROpHET/static/media/splash.c0493b08.png"
+  }
+]);
