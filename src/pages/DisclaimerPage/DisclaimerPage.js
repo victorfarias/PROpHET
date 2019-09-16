ab1c82a3c0ed155944438a3e87f4ce7959f4d6dd
victor <victor.aefarias@gmail.com>
2019-09-15 20:35:20 -0400

    disclaimer udpate

--- src/pages/DisclaimerPage/DisclaimerPage.js
+++ src/pages/DisclaimerPage/DisclaimerPage.js
@@ -14,22 +14,29 @@ function DisclaimerPage(props) {
                 <div className="text">
                     <h1 className="my-5">The Disclaimer</h1>
                     <span className="py-4">
-                        This application is made available by Universidade
-                        Federal de Sao Paulo (Brazil) and Hospital Geral de
-                        Fortaleza (Brazil), in collaboration with staff of
-                        Universidade Federal do Ceara (Brazil)  
-                         {/* and Columbia University (USA),  */} 
-                         solely for personal non-commercial,
-                        teaching and educational use. This application is not a
-                        medical device and does not and should not be construed
-                        to provide health-related or medical advice, or clinical
-                        decision support, or to support or replace the
-                        diagnosis, or other kind of medical decision. This
-                        application does not create a physician-patient
-                        relationship between the above-mentioned institutions
-                        and any individual. You, the user, agree to use this
-                        application under understanding about all these terms
-                        and conditions.
+                        This application is made available solely for personal
+                        non-commercial, teaching and educational use. This
+                        application is not a medical device and does not and
+                        should not be construed to provide health-related or
+                        medical advice, or clinical decision support, or to
+                        support or replace the diagnosis, or other kind of
+                        medical decision. This application does not create a
+                        physician-patient relationship between the
+                        above-mentioned institutions and any individual. You,
+                        the user, agree to use this application under all these
+                        terms and conditions. If you do not agree to all of
+                        these terms of use, do not use this site.
+                        <br />
+                        Dr. Joao Andrade and the developers make no
+                        representations as to any matter whatsoever, including
+                        accuracy, fitness for a particular purpose or
+                        non-infringement.
+                        <br />
+                        Dr Joao Andrade and the developers are not liable to you
+                        or anyone else for any decision made or action taken
+                        based on reliance upon the information contained on or
+                        provided through this website. The use of the
+                        information shall be at your own risk.
                     </span>
                 </div>
             </div>
